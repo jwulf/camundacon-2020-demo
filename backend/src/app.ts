11b086e89dc78a06e37f7d907ed5c3417197476c
Josh Wulf <josh@magikcraft.io>
2020-04-22 20:11:09 +1200

    Cors update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,15 +23,15 @@ zbc.createWorker({
 app.use(bodyParser.json());
 
 app.options("*", cors()); // include before other routes
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.post("/", async (req, res) => {
   zbc
